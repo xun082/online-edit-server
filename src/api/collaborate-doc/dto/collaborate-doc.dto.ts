06d2aa8ba4a5ab4c48a5a2a8d695b7681bf94f64
Vinnyaaaa <dongshengliu98@gmail.com>
2024-10-27 14:12:13 +0800

    feat:add share link

--- src/api/collaborate-doc/dto/collaborate-doc.dto.ts
+++ src/api/collaborate-doc/dto/collaborate-doc.dto.ts
@@ -1,7 +1,21 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class DetailDto {
   @IsNotEmpty()
   @IsString()
   recordId: string;
+}
+
+export class CreateShareLinkDto {
+  @IsString()
+  recordId: string;
+
+  @IsString()
+  @IsOptional()
+  accessLevel?: string;
+}
+
+export class ShareDetailDto {
+  @IsString()
+  shareId: string;
 }
