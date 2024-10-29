fa83e5765e6c0a58cc3e6563c3516ced34e00643
xun082 <2042204285@qq.com>
2024-10-29 13:04:00 +0800

    feat: 🚀 添加自动化部署

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Model, Types } from 'mongoose';
 import { ObjectId } from 'mongodb';
 import { InjectModel } from '@nestjs/mongoose';
-import * as argon2 from 'argon2';
+import * as bcrypt from 'bcrypt';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { plainToInstance } from 'class-transformer';
 
@@ -42,23 +42,19 @@ export class UserService {
   ) {}
 
   async getUserInfo(currentUserId: string, userId?: string): Promise<UserWithFriendStatusDto> {
-    // 如果 userId 是 undefined，则使用 currentUserId
     const targetUserId = userId || currentUserId;
 
-    // 获取用户信息
     const user = await this.userModel
       .findOne({ _id: targetUserId })
       .select('-password')
       .lean()
       .exec();
 
-    // 检查是否为好友
     const isFriend =
       targetUserId === currentUserId
-        ? true // 如果查询的是自己的信息，直接设置为 true
+        ? true
         : await this.areUsersFriends(currentUserId, targetUserId);
 
-    // 返回组合后的数据
     return {
       ...user,
       isFriend,
@@ -74,8 +70,6 @@ export class UserService {
         ],
       })
       .exec();
-
-    console.log(friendship);
 
     return !!friendship;
   }
@@ -97,32 +91,28 @@ export class UserService {
   async createUserByEmail(data: createUserDto, isLoginType?: boolean) {
     const { email, code, password, confirm_password } = data;
 
-    // 检查密码和确认密码是否匹配
     if (password !== confirm_password) {
       throw new ValidationException('Passwords do not match');
     }
 
-    // 加密密码
-    const passwordEncryption = await argon2.hash(password);
+    // 使用 bcrypt 加密密码
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-    // 如果是登录类型，直接创建新用户
     if (isLoginType) {
-      return await this.createAndSaveUser(email, passwordEncryption, 'moment');
+      return await this.createAndSaveUser(email, passwordHash, 'moment');
     }
 
-    // 检查验证码是否匹配
     const uniqueId = await this.redisService.get(email);
 
     if (uniqueId !== code) {
       throw new ValidationException('Verification code is incorrect');
     }
 
-    // 查找是否已有用户
     const existingUser = await this.userModel.findOne({ email }).lean().exec();
 
-    // 如果没有找到用户，创建新用户
     if (!existingUser) {
-      return await this.createAndSaveUser(email, passwordEncryption, 'moment');
+      return await this.createAndSaveUser(email, passwordHash, 'moment');
     }
   }
 
@@ -149,18 +139,14 @@ export class UserService {
     senderId: string,
     createFriendRequestDto: CreateFriendRequestDto,
   ): Promise<ResponseDto<void>> {
-    console.log(senderId);
-
     const { receiverId } = createFriendRequestDto;
 
-    // 检查要申请的用户是否存在
     const senderExists = await this.userModel.exists({ _id: receiverId });
 
     if (!senderExists) {
       throw new ValidationException('申请的用户不存在');
     }
 
-    // 检查发送者和接收者是否已经是好友
     const existingFriendship = await this.friendModel
       .findOne({ user_id: senderId, friend_id: receiverId })
       .exec();
@@ -169,7 +155,6 @@ export class UserService {
       throw new ValidationException('You are already friends with this user');
     }
 
-    // 检查是否已经存在未处理的好友请求
     const existingRequest = await this.friendRequestModel
       .findOne({
         senderId,
@@ -178,7 +163,6 @@ export class UserService {
       .exec();
 
     if (existingRequest) {
-      // 更新现有的好友请求
       existingRequest.description = createFriendRequestDto.description;
       existingRequest.createdAt = getCurrentTimestamp();
       await existingRequest.save();
@@ -192,7 +176,6 @@ export class UserService {
         }),
       );
     } else {
-      // 创建新的好友请求
       const friendRequest = new this.friendRequestModel({
         senderId: senderId,
         ...createFriendRequestDto,
@@ -227,7 +210,6 @@ export class UserService {
   }
 
   async getFriendsList(userId: string): Promise<FriendDetailsDto[]> {
-    // Step 1: 获取好友关系数据，包括 user_id 或 friend_id 为当前用户的关系
     const friends = await this.friendModel
       .find({
         $or: [{ user_id: userId }, { friend_id: userId }],
@@ -236,22 +218,18 @@ export class UserService {
       .lean()
       .exec();
 
-    // Step 2: 提取所有与当前用户相关的用户ID
     const friendIds = friends.map((friend) =>
       friend.user_id.toString() === userId ? friend.friend_id : friend.user_id,
     );
 
-    // Step 3: 查找 friend_id 对应的用户详细信息
     const users = await this.userModel
       .find({ _id: { $in: friendIds } })
       .select('email username avatar')
       .lean()
       .exec();
 
-    // Step 4: 创建一个用户信息映射表
     const userMap = new Map(users.map((user) => [user._id.toString(), user]));
 
-    // Step 5: 组合好友记录和用户详细信息
     const result = friends
       .map((friend) => {
         const friendId = friend.user_id.toString() === userId ? friend.friend_id : friend.user_id;
@@ -262,8 +240,8 @@ export class UserService {
 
         if (user) {
           return {
-            id: friend._id.toString(), // 将 ObjectId 转换为字符串
-            friendId: friendId.toString(), // 将 ObjectId 转换为字符串
+            id: friend._id.toString(),
+            friendId: friendId.toString(),
             friendEmail: user.email,
             friendUsername: user.username,
             friendRemark: remark,
@@ -274,30 +252,24 @@ export class UserService {
 
         return null;
       })
-      .filter((friend) => friend !== null); // 过滤掉 null 项
+      .filter((friend) => friend !== null);
 
     return result as FriendDetailsDto[];
   }
 
-  // 通过好友验证
   async updateFriendRequestStatus(
     requestId: string,
     updateFriendRequestDto: UpdateFriendRequestStatusDto,
   ): Promise<ResponseDto<void>> {
     const friendRequest = await this.friendRequestModel.findOne({ senderId: requestId }).exec();
 
-    console.log(friendRequest);
-
     if (!friendRequest) {
       throw new ValidationException('Friend request not found');
     }
 
-    // 检查状态是否合理
     if (friendRequest.status !== FriendRequestStatus.PENDING) {
       throw new ValidationException('Friend request is not pending');
     }
-
-    // await friend.save();
 
     this.eventEmitter.emit(
       SocketKeys.FRIEND_REQUEST_UPDATED,
@@ -311,14 +283,9 @@ export class UserService {
     return;
   }
 
-  // 修改用户信息
   async updateUserInfo(userId: string, updateUserDto: UpdateUserDto): Promise<void> {
     const updatedUser = await this.userModel
-      .findByIdAndUpdate(
-        userId,
-        { $set: updateUserDto }, // 使用 $set 只更新传递的字段
-        { new: true, runValidators: true }, // new: true 返回更新后的文档，runValidators: true 运行更新的验证器
-      )
+      .findByIdAndUpdate(userId, { $set: updateUserDto }, { new: true, runValidators: true })
       .exec();
 
     if (!updatedUser) {
